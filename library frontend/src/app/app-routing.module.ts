6c538443c30e85b49b48e7ac94e3dcf7a2b3e8c4
iamsafin <ahmedshafinshafi@gmail.com>
2024-01-20 21:54:18 +0600

    ebook module created

--- library frontend/src/app/app-routing.module.ts
+++ library frontend/src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { PagesRegisterComponent } from './pages/auth-module/pages-register/pages
 import { UsersProfileComponent } from './pages/users-profile/users-profile.component';
 
 const routes: Routes = [
-{
+  {
     path: 'book-issue',
     loadChildren: () =>
       import('./modules/bookissue-module/bookissue-module.module').then(
@@ -21,6 +21,13 @@ const routes: Routes = [
       ),
   },
   {
+    path: 'ebook',
+    loadChildren: () =>
+      import('./modules/ebook-module/ebook-module.module').then(
+        (m) => m.EbookModuleModule
+      ),
+  },
+  {
     path: 'customer',
     loadChildren: () =>
       import('./modules/customer-module/customer-module.module').then(
@@ -28,18 +35,17 @@ const routes: Routes = [
       ),
   },
 
- { path: '', component: DashboardComponent },
-{ path: 'dashboard', component: DashboardComponent },
-{ path: 'pages-login', component: PagesLoginComponent },
-{ path: 'pages-register', component: PagesRegisterComponent },
-{ path: 'user-profile', component: UsersProfileComponent },
-// { path: 'book-issue', component: BookIssueComponent },
-// { path: 'create', component: BookIssueComponent },
-
+  { path: '', component: DashboardComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'pages-login', component: PagesLoginComponent },
+  { path: 'pages-register', component: PagesRegisterComponent },
+  { path: 'user-profile', component: UsersProfileComponent },
+  // { path: 'book-issue', component: BookIssueComponent },
+  // { path: 'create', component: BookIssueComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ library frontend/src/app/modules/ebook-module/ebook-module-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { EbookComponent } from './ebook/ebook.component';
+import { EbookCreateComponent } from './ebook/ebookcreate.component';
+
+const routes: Routes = [
+  { path: 'index', component: EbookComponent },
+  { path: 'create', component: EbookCreateComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class EbookModuleRoutingModule {}
